68e2b1855d487cec1feec0e33de9047e873bc6c8
minji-dan <aksen569@naver.com>
2022-06-06 20:33:06 +0900

    feat: home에 음성 지원 기능 추가

--- DanGuideApp/app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
+++ DanGuideApp/app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -1,32 +1,53 @@
 package com.example.myapplication.ui.home;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
+    TextView tv;
+    MediaPlayer mp;
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View view = clickEvent(inflater, container);
+        return view;
+    }
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+    public View clickEvent(LayoutInflater inflater, ViewGroup container) {
+        mp = MediaPlayer.create(getActivity(), R.raw.map);
+        mp.setLooping(false);
+
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        tv = (TextView) v.findViewById(R.id.hellobutton);
+        tv.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mp.isPlaying()) {
+                    mp.start();
+                    tv.setText("Stop");
+                } else {
+                    mp.pause();
+                    tv.setText("Start");
+                }
+
+            }
+        });
+        return v;
     }
 
     @Override
