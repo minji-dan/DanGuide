ae7462957e365c86bc68b40b3edad06d97e5a988
FireMelonJuice <dslee6544@gmail.com>
2022-06-07 21:29:18 +0900

    실내 길찾기 기능 추가

--- DanGuideApp/app/src/main/java/com/example/myapplication/ui/indoor/IndoorFragment.java
+++ DanGuideApp/app/src/main/java/com/example/myapplication/ui/indoor/IndoorFragment.java
@@ -1,10 +1,14 @@
 package com.example.myapplication.ui.indoor;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -19,14 +23,16 @@ public class IndoorFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        IndoorViewModel notificationsViewModel =
+        IndoorViewModel indoorViewModel =
                 new ViewModelProvider(this).get(IndoorViewModel.class);
 
         binding = FragmentIndoorBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final WebView webView = binding.indoorNavi;
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://10.0.2.2:3000/indoor");
+
         return root;
     }
 
